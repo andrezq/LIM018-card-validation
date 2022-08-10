d7bd66fbac057a03746e62062d85738005cf8fb1
Victor Z <110549464+andrezq@users.noreply.github.com>
2022-08-10 08:00:27 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import validator from './validator.js';
+const tarjeta = document.querySelector('#tarjeta');
+btnAbrirFormulario = document.querySelector('#btn-abrir-formulario')
+formulario = document.querySelector('#tformulario-tarjeta');
 
-console.log(validator);
+Document.getElementById(tarjeta);
+tarjeta.addEventListener('click', () => {
+    tarjeta.classList.toggle('active');
+});
+
+btnAbrirFormulario.addEventListener('click', () => {
+    tarjeta.classList.toggle('active');
+    formulario.classList.toggle('active');
+});
+
+for(let i=1; i <= 12; i++){
+    let opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.innerText = i;
+    formulario.selectMes.appendChild(opcion)
+} 
