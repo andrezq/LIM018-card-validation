84dd01ba2e3cd703c5c7d80384b0cf66b47cf570
Victor Z <110549464+andrezq@users.noreply.github.com>
2022-08-11 00:25:03 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,34 +1,9 @@
-const tarjeta = document.querySelector('#tarjeta');
-btnAbrirFormulario = document.querySelector('#btn-abrir-formulario');
-formulario = document.querySelector('#tformulario-tarjeta');
+  function voltearTarjeta () {
+    const tarjeta = document.querySelector('#tarjeta');
+    tarjeta.classList.add('active')
+  } 
 
-tarjeta.addEventListener('click', () => {
-    tarjeta.classList.toggle('active');
-});
-
-btnAbrirFormulario.addEventListener('click', () => {
-    tarjeta.classList.toggle('active');
-    formulario.classList.toggle('active');
-});
-
-for(let i=1; i <= 12; i++){
-    let opcion = document.createElement('option');
-    opcion.value = i;
-    opcion.innerText = i;
-    formulario.selectMes.appendChild(opcion)
-} 
-const yearActual = new Date().getFullYear();
-for(let i = yearActual; i <= yearActual + 8; i++){
-    let opcion = document.createElement('option');
-    opcion.value = i;
-    opcion.innerText = i;
-    formulario.selectYear.appendChild(opcion);
-}
-formulario.inputNumero.addEventListener('keyup', (e) => {
-    let valorInput = e.target.value;
-
-    formulario.inputNumero.value = valorinput
-    // Eliminamos espacios en blanco
-    .replace(/\s/g, '');
-    .replace(/\D/g, '')
-})
+  function abrirFormulario () {
+    btnAbrirFormulario = document.querySelector('#btn-abrir-formulario');
+    btnAbrirFormulario.classList.add('active');
+  } 
