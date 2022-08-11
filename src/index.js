d23e0249d372e8e36022d76c533a34e826c88eeb
Victor Z <110549464+andrezq@users.noreply.github.com>
2022-08-10 21:35:41 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const tarjeta = document.querySelector('#tarjeta');
-btnAbrirFormulario = document.querySelector('#btn-abrir-formulario')
+btnAbrirFormulario = document.querySelector('#btn-abrir-formulario');
 formulario = document.querySelector('#tformulario-tarjeta');
 
-Document.getElementById(tarjeta);
 tarjeta.addEventListener('click', () => {
     tarjeta.classList.toggle('active');
 });
@@ -18,3 +17,18 @@ for(let i=1; i <= 12; i++){
     opcion.innerText = i;
     formulario.selectMes.appendChild(opcion)
 } 
+const yearActual = new Date().getFullYear();
+for(let i = yearActual; i <= yearActual + 8; i++){
+    let opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.innerText = i;
+    formulario.selectYear.appendChild(opcion);
+}
+formulario.inputNumero.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+
+    formulario.inputNumero.value = valorinput
+    // Eliminamos espacios en blanco
+    .replace(/\s/g, '');
+    .replace(/\D/g, '')
+})
